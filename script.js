0ba244289c5a7207c6a7968e427270240d92ce56
Zelouchan <bad1dje@gmail.com>
2025-01-27 17:37:23 +0000

    working on toggle read

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let books = [];
 
+// gather information about books and order it
 function Info(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -7,8 +8,9 @@ function Info(title, author, pages, read) {
   this.read = read;
 }
 
+// events occuring when the submit button is clicked
 document.getElementById("submit").addEventListener("click", function (event) {
-  event.preventDefault();
+  event.preventDefault(); //makes sure that the page doesn't refresh
 
   const bookTitle = document.getElementById("title").value;
   const bookAuthor = document.getElementById("author").value;
@@ -17,27 +19,39 @@ document.getElementById("submit").addEventListener("click", function (event) {
 
   const book = new Info(bookTitle, bookAuthor, bookPages, bookRead);
 
+  // updates the array
   books.push(book);
 
+  //calls the function that displays the books
   displayBooks();
 
+  //clears the fields in the form
   document.getElementById("myForm").reset();
 });
 
+//loop that makes sure the books are displaye
 function displayBooks() {
   const shelf = document.getElementById("shelf");
 
+//clears anything on the page
   shelf.innerHTML= "";
 
+  //loop that generates the updates book information to the screen
   books.forEach(book => {
-
   const newDiv = document.createElement("div");
   newDiv.innerHTML = `
   <strong>Title:</strong> ${book.title}<br>
   <strong>Author:</strong> ${book.author}<br>
   <strong>Pages:</strong> ${book.pages}<br>
-  <strong>Read?</strong> ${book.read}`;
+  <strong>Read?</strong> ${book.read}
+`;
 
   shelf.appendChild(newDiv); });
+}
+
+function toggleRead() {
+  const bookRead = document.getElementById("book_read") 
+  books[bookIndex].read = !books[bookIndex].read;
+  displayBooks();
 }
 
