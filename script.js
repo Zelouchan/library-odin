e605b150c864780fbb6ca5c786b5009b6b017efd
Zelouchan <bad1dje@gmail.com>
2025-01-27 15:23:18 +0000

    Added Javascript

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-function info(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.sayTitle = function() {
-      return this.title + ", " + this.author + ", " + this.pages + ", " + this.read;
-    };
-  }
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookPages = document.getElementById("pages");
+const bookRead = document.getElementById("book_read");
 
-  const book = new info("The Hobbit", "JRR Tolkien", "297 pages", "Read");
+function Info(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.sayTitle = function() {
+    return this.title + ", " + this.author + ", " + this.pages + ", " + this.read;
+  };
+}
+
+document.getElementById("submit").addEventListener("click", function(event) {
+  event.preventDefault();
+
+  const book = new Info(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
   console.log(book.sayTitle());
+
+  const newDiv = document.createElement("div");
+  newDiv.innerHTML = `
+  <strong>Title:</strong> ${book.title}<br>
+  <strong>Author:</strong> ${book.author}<br>
+  <strong>Pages:</strong> ${book.pages}<br>
+  <strong>Status:</strong> ${book.read ? "Read" : "Not Read"}`;
+
+  const parentElement = document.getElementById("shelf"); 
+  parentElement.appendChild(newDiv);
+
+  const form = getElementById("myForm")
+  form.reset();
+})
+
+
+
