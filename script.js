703a2a5e763f63b809b05bcf4a95732856d9befe
Zelouchan <bad1dje@gmail.com>
2025-01-27 19:34:46 +0000

    No clue what i am doing at this point

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ function Info(title, author, pages, read) {
 
 // events occuring when the submit button is clicked
 document.getElementById("submit").addEventListener("click", function (event) {
-  event.preventDefault(); //makes sure that the page doesn't refresh
-
   const bookTitle = document.getElementById("title").value;
   const bookAuthor = document.getElementById("author").value;
   const bookPages = document.getElementById("pages").value;
@@ -33,25 +31,29 @@ document.getElementById("submit").addEventListener("click", function (event) {
 function displayBooks() {
   const shelf = document.getElementById("shelf");
 
-//clears anything on the page
-  shelf.innerHTML= "";
+  //clears anything on the page
+  shelf.innerHTML = "";
 
   //loop that generates the updates book information to the screen
-  books.forEach(book => {
-  const newDiv = document.createElement("div");
-  newDiv.innerHTML = `
+  books.forEach((book) => {
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
   <strong>Title:</strong> ${book.title}<br>
   <strong>Author:</strong> ${book.author}<br>
   <strong>Pages:</strong> ${book.pages}<br>
-  <strong>Read?</strong> ${book.read}
+  <strong>Read?</strong> ${book.read} <br>
+  <label class="switch">
+  <input type="checkbox">
+  <span class="slider round"></span>
+  <button id="delete">delete</button> 
+</label>
 `;
 
-  shelf.appendChild(newDiv); });
+    shelf.appendChild(newDiv);
+  });
 }
 
 function toggleRead() {
-  const bookRead = document.getElementById("book_read") 
-  books[bookIndex].read = !books[bookIndex].read;
+  // const bookRead = document.getElementById("book_read")
   displayBooks();
 }
-
